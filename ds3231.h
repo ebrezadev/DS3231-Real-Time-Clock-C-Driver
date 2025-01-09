93ea1b51318c432210d6b1cb3224eff77e8c5592
Mason Hieb <79335632+masonhieb@users.noreply.github.com>
2025-01-09 16:51:27 -0600

    Update ds3231.h

--- ds3231.h
+++ ds3231.h
@@ -94,9 +94,9 @@ uint8_t ds3231_run_command(uint8_t command);
 uint8_t ds3231_run_status();
 
 void ds3231_I2C_init();
-void time_i2c_write_single(uint8_t device_address, uint8_t register_address, uint8_t data_byte);
+void time_i2c_write_single(uint8_t device_address, uint8_t register_address, uint8_t *data_byte);
 void time_i2c_write_multi(uint8_t device_address, uint8_t start_register_address, uint8_t *data_array, uint8_t data_length);
-void time_i2c_read_single(uint8_t device_address, uint8_t register_address, uint8_t data_byte);
+void time_i2c_read_single(uint8_t device_address, uint8_t register_address, uint8_t *data_byte);
 void time_i2c_read_multi(uint8_t device_address, uint8_t start_register_address, uint8_t *data_array, uint8_t data_length);
 
 #endif
